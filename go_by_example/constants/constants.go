c6454c6fdd785c4d77068be600a3c6660ef880d8
Matt Mintzer <mintzer.chemistry@gmail.com>
2022-01-05 16:33:21 -0500

    add: variables and constants

---
+++ go_by_example/constants/constants.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	const c = "We have consts in this language"
+
+	fmt.Println(c)
+}
---
+++ go_by_example/variables/variables.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := "coolest thing I learned here was \":=\" being able to assign variables with implicit typing"
+	fmt.Println(a)
+}
