3b3346156ca83dfb9e772efdee45b61f11aea080
Matt Mintzer <mintzer.chemistry@gmail.com>
2022-01-06 16:59:20 -0500

    add: switch example

---
+++ go_by_example/switch/switch.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 4
+
+	switch i {
+	case 4:
+		fmt.Println("Set a variable, i, to the int 4.  Make a switch statement based on i use the case 4 to print this.")
+	}
+}
