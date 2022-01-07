2dacc54646b9ba6db0c79655f4b861ea1829723c
Matt Mintzer <mintzer.chemistry@gmail.com>
2022-01-07 15:06:21 -0500

    add: arrays

---
+++ go_by_example/arrays/arrays.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// An array is a numbered sequence of elements of specific length
+// THe type of elemends and the length are both
+// part of the arrays type
+// by default an array is zero-valued
+
+func main() {
+	// syntax a little funky to decalare and initialize
+	a := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(a)
+
+	// usually going to be working with slices
+}
